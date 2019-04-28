c540d6de6f2789992649421d6c9e30a80c94d73e
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2019-04-28 22:04:47 +0100

    sending static data

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { parse } = require("url");
-
 module.exports = (req, res) => {
-    const { query } = parse(req.url, true);
-    const { name = "World" } = query;
-    res.end(`Hello ${name}!`);
+    res.send([
+        { "tier": "1", "nodes": "585", "roi": "100%" },
+        { "tier": "2", "nodes": "324", "roi": "100.31%" },
+        { "tier": "3", "nodes": "213", "roi": "91.39%" },
+        { "tier": "4", "nodes": "147", "roi": "79.46%" }
+    ]);
 };
