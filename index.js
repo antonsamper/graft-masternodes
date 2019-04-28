fdfea9bcb1a35373c880e4f00c99fe621f470059
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2019-04-28 22:41:49 +0100

    setting content-type

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 module.exports = (req, res) => {
-    res.json({"test": "111"});
+    res.set('Content-Type', 'application/json');
+    res.end([
+        { "tier": "1", "nodes": "585", "roi": "100%" },
+        { "tier": "2", "nodes": "324", "roi": "100.31%" },
+        { "tier": "3", "nodes": "213", "roi": "91.39%" },
+        { "tier": "4", "nodes": "147", "roi": "79.46%" }
+    ]);
 };
