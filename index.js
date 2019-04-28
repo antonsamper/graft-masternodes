352df21c338729c9fa9488d52b0d9917d0a1a2e2
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2019-04-28 22:23:20 +0100

    cleaning response

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = (req, res) => {
-    res.set('Content-Type', 'application/json');
-    res.status(200).send([
+    res.end([
         { "tier": "1", "nodes": "585", "roi": "100%" },
         { "tier": "2", "nodes": "324", "roi": "100.31%" },
         { "tier": "3", "nodes": "213", "roi": "91.39%" },
