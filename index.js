8f235bbe78046cd3c40683529e524bd16172b802
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2019-04-29 07:17:18 +0100

    wrapping res in sedn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const got = require('got');
+const { send } = require('micro');
 
 module.exports = async (req, res) => {
     try {
         res.setHeader('Content-Type', 'application/json');
-        return await got(process.env.SUPERNODE_LIST);
+
+        const data = got(process.env.SUPERNODE_LIST);
+        send(res, 200, data)
     } catch (err) {
         send(res, 502, 'error fetching supernode list')
     }
