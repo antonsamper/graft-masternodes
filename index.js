05b58f9df278a2a62484d873589c5e89fa14ebc4
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2019-04-29 09:22:16 +0100

    checking if stake is valid

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ module.exports = async (req, res) => {
         let tier4Nodes = 0;
 
         nodes.forEach((node) => {
+            if(node.IsStakeValid) {
             const stake = Math.ceil(node.StakeAmount / 10000000000);
-
-            if (stake > 250000) { tier4Nodes++ }
-            else if (stake > 150000) { tier3Nodes++ }
-            else if (stake > 100000) { tier2Nodes++ }
-            else if (stake > 50000) { tier1Nodes++ }
-            else { tier0Nodes++ }
+                if (stake >= 250000) { tier4Nodes++ }
+                else if (stake >= 150000) { tier3Nodes++ }
+                else if (stake >= 100000) { tier2Nodes++ }
+                else if (stake >= 50000) { tier1Nodes++ }
+                else { tier0Nodes++ }
+            }
         });
 
         res.setHeader('Content-Type', 'application/json');
